a6e9819ac93851a426c62c6dd2fdc67d5bf06d3d
uretral <cdnadom@gmail.com>
2017-04-18 21:29:23 +0400

    Add files via upload

---
+++ uretral/users.list/lang/ru/.description.php
@@ -0,0 +1,6 @@
+<?
+$MESS['USERS_LIST_DESCRIPTION_NAME'] = 'Кастомный компонент';
+$MESS['USERS_LIST_DESCRIPTION_DESCRIPTION'] = 'Компанент для отображения списка пользователей';
+$MESS['USERS_LIST_DESCRIPTION_GROUP'] = 'Тестовые';
+$MESS['USERS_LIST_DESCRIPTION_DIR'] = 'Кастомный компонент users.list';
+?>
---
+++ uretral/users.list/lang/ru/.parameters.php
@@ -0,0 +1,11 @@
+<?
+$MESS['USERS_LIST_ORDER_BY_FIELD'] = 'Поле сортировки';
+$MESS['USERS_LIST_ORDER_BY_DIRECTION'] = 'Направление сортировки';
+$MESS['USERS_LIST_ORDER_BY_ID'] = 'ID пользователя';
+$MESS['USERS_LIST_ORDER_BY_LOGIN'] = 'Логин пользователя';
+$MESS['USERS_LIST_ORDER_BY_EMAIL'] = 'Email пользователя';
+$MESS['USERS_LIST_ORDER_BY_NAME'] = 'Имя пользователя';
+$MESS['USERS_LIST_SHOW_NAV'] = 'Постраничная навигация';
+$MESS['USERS_LIST_PARAMETERS_COUNT'] = 'Количество элементов';
+$MESS['USERS_LIST_SORT_ASC'] = 'По возрастанию';
+$MESS['USERS_LIST_SORT_DESC'] = 'По убыванию';
